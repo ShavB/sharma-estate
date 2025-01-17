7b0c9f1ec5c5e72172dee6bd5549613471425566
Shyam Bisht <shav@MAC-B6751F.station>
2025-01-17 21:25:37 +0000

    Setup for APP and added header for the application

---
+++ app/_components/Header.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Header() {
+  const path = usePathname();
+  useEffect(() => {
+    console.log(path);
+  });
+  return (
+    <div className="flex justify-between p-4 px-10 shadow fixed w-full top-0 bg-white">
+      <div className="flex gap-12 items-center">
+        <Image src={"/logo.svg"} width={60} height={60} alt="sharmaEstate" />
+        <ul className="hidden md:flex gap-10">
+          <Link href={"/"}>
+            <li
+              className={`"hover:text-primary font-medium text-medium cursor-pointer"
+                ${path == "/" && "text-primary"}`}
+            >
+              For Sale
+            </li>
+          </Link>
+          <Link href={"/"}>
+            <li className="hover:text-primary font-medium text-medium cursor-pointer">
+              For Rent
+            </li>
+          </Link>
+          <Link href={"/"}>
+            <li className="hover:text-primary font-medium text-medium cursor-pointer">
+              Agent Finder
+            </li>
+          </Link>
+        </ul>
+      </div>
+      <div className="flex gap-2 items-center">
+        <Button className="flex gap-2">
+          <Plus className="h-5 w-5" /> Post New Add
+        </Button>
+        <Button className="" variant="outline">
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
